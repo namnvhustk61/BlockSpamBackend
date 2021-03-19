05af516a644ba796f5477d3ff4562c0747c126a2
NamReal <namrealbk10@gmail.com>
2021-03-19 14:00:30 +0700

    contet-type

--- routes/middleware/mid_check_content_type.js
+++ routes/middleware/mid_check_content_type.js
@@ -6,7 +6,8 @@ const Strings = require("../../bin/Strings.js");
 module.exports = {
 
     json: function (req, res, next) {
-            if(req.headers["content-type"] == "application/json"){
+        var x = ""
+            if(req.headers["content-type"].includes( "application/json")){
                 next();
             }else{
                 res.json(Result.create(Result.E_001, Strings.E_001_MESS, null));
